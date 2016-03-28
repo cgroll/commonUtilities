74e7f114167d31f1307fe408de53b6acbc66d24b
cgroll <groll.christian.edu@gmail.com>
2016-03-28 16:42:24 +0200

    plot time series table

--- tableFuncs/plotTable.m
+++ tableFuncs/plotTable.m
@@ -1,5 +1,14 @@
-function plotTable(tableToPlot)
+function plotTable(tableToPlot, varargin)
+% plot table with time series data as row names
+%
+% Inputs:
+%   tableToPlot     nxm table with dates as row names
+
+% convert strings to numeric dates
 serialDates = datenum(tableToPlot.Properties.RowNames);
-plot(serialDates, tableToPlot{:, :})
+
+% plot with dates on x axis
+plot(serialDates, tableToPlot{:, :}, varargin{:})
 datetick 'x'
+
 end
