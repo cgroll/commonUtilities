45274aca971d04c41b59c2931986af7e87c40b5b
cgroll <groll.christian.edu@gmail.com>
2016-04-25 16:48:17 +0200

    table functions added

---
+++ tableFuncs/hcatUnequalTables.m
@@ -0,0 +1,40 @@
+function fullTab = hcatUnequalTables(tab1, tab2)
+% appending table with subset of columns
+%
+% Inputs:
+%   tab1    nxm table
+%   tab2    lxk table, k<m
+%
+% Outputs:
+%   fullTab     (n+l)xm table
+%
+% Remarks:
+% - columns of tab2 must be subset of columns of tab1
+% - column sorting will be given by larger table tab1
+
+% check if all columns of table 2 do occur in table 1
+if all(ismember(tabnames(tab2), tabnames(tab1))) == false
+    error('commonUtil:hcatUnequalTables',...
+            'Columns of table 2 must be subset of table 1.')
+end
+
+% preallocate new table with full number of columns
+nObs = size(tab2, 1);
+nVars = size(tab1, 2);
+
+bigTab2 = array2table(NaN(nObs, nVars), 'VariableNames', tabnames(tab1));
+
+colNames = tabnames(tab1);
+for ii=1:nVars
+    thisCol = colNames(ii);
+
+    if ismember(thisCol, tabnames(tab2))
+        % get values, if column exists in tab2
+        bigTab2(:, thisCol) = tab2(:, thisCol);
+    end
+end
+
+% append table
+fullTab = [tab1; bigTab2];
+
+end
---
+++ tableFuncs/tabnames.m
@@ -0,0 +1,6 @@
+function nams = tabnames(tab)
+% get table names
+
+nams = tab.Properties.VariableNames;
+
+end
