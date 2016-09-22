1b1b25694608ee4dfd5bd9bce1be2e6aa496402c
cgroll <groll.christian.edu@gmail.com>
2016-09-22 18:52:37 +0200

    own implementation of moving average

---
+++ returnFuncs/movingAvg.m
@@ -0,0 +1,15 @@
+function thisMvAvg = movingAvg(dataVals, windowSize, skipWarmup)
+% apply moving average to vector of data vals
+
+% specify filter
+dataWgts = 1/windowSize * ones(1, windowSize);
+
+% apply filter
+thisMvAvg = filter(dataWgts, 1, dataVals);
+
+% treat first values as NaNs
+if skipWarmup
+    thisMvAvg(1:(windowSize-1)) = NaN;
+end
+
+end
